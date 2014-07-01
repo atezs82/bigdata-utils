4372627314b90529ca1123a50f1a5d1342360ddd
Attila Toth <attila.toth@ericsson.com>
2014-07-01 19:08:59 -0400

    ADD: bunch of initial things to "infinispan inquisitor" module. does sounds bad, might rename in the future

---
+++ infinispan-inquisitor/src/main/java/org/zenworks/infinispan/inquisitor/InfinispanMainWindow.java
@@ -0,0 +1,4 @@
+package org.zenworks.infinispan.inquisitor;
+
+public class InfinispanMainWindow {
+}
---
+++ infinispan-inquisitor/src/main/java/org/zenworks/infinispan/inquisitor/InfinispanWindowController.java
@@ -0,0 +1,36 @@
+package org.zenworks.infinispan.inquisitor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class InfinispanController {
+
+    @FXML
+    private Button queryKey;
+
+    @FXML
+    private ComboBox<?> ipanHost;
+
+    @FXML
+    private TextArea ipanContent;
+
+    @FXML
+    private Button connectToIpan;
+
+    @FXML
+    private TextField ipanKey;
+
+    @FXML
+    void onConnectToIpan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onQueryKey(ActionEvent event) {
+
+    }
+
+}
